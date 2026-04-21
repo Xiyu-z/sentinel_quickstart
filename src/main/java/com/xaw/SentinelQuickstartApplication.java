c049d06bfba1405b68e6341b7de3515e7c15763d
xiyv <xaw3205443920999@163.com>
2026-04-21 14:56:35 +0800

    sentinel入门-完结

---
+++ src/main/java/com/xaw/SentinelQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.xaw.sentinel_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelQuickstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xaw/SentinelQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaw.sentinel_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
